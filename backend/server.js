7b9855c300f3a1307b36e676b482a1879867fe4a
Caio Gullo <caioinacio19@gmail.com>
2025-08-21 19:36:48 -0300

    fix: moving db

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 const express = require('express');
-const { sequelize, testConnection } = require('./db/db.js');
+const { sequelize, testConnection } = require('./db');
 
 const app = express();
 app.use(express.json());
