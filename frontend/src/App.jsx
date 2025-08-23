52ece776230d1e843c81180a2280f1d09e0fbec1
Caio Gullo <caioinacio19@gmail.com>
2025-08-23 15:17:47 -0300

    feat: atualizando paths

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
-import Login from './pages/Login';
-import Signup from './pages/Signup';
-import Home from './pages/Home';
+import Login from './pages/Login/Login';
+import Signup from './pages/Signup/Signup';
+import Home from './pages/Home/Home';
 
 function PrivateRoute({ children }) {
   const token = localStorage.getItem('token');
