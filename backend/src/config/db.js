4e76e51496065d24dace9b2cde98628e75486007
Caio Gullo <caioinacio19@gmail.com>
2025-08-23 14:56:34 -0300

    feat(db): usando postgre em prod

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,14 +1,29 @@
 require('dotenv').config();
 const { Sequelize } = require('sequelize');
-const path = require('path');
 
-const storage = process.env.DB_STORAGE || path.join(__dirname, '../../db.sqlite');
+let sequelize;
 
-const sequelize = new Sequelize({
-  dialect: process.env.DB_DIALECT || 'sqlite',
-  storage,
-  logging: false,
-});
+if (process.env.DATABASE_URL) {
+  sequelize = new Sequelize(process.env.DATABASE_URL, {
+    dialect: 'postgres',
+    protocol: 'postgres',
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false 
+      }
+    },
+    logging: false,
+  });
+} else {
+  const path = require('path');
+  const storage = process.env.DB_STORAGE || path.join(__dirname, '../../db.sqlite');
+  sequelize = new Sequelize({
+    dialect: 'sqlite',
+    storage,
+    logging: false,
+  });
+}
 
 async function testConnection() {
   try {
